a12aa1fc6f5683f5bfc547c4db1f382c9567b393
panchi007 <vitobernal@hotmail.com>
2024-04-25 16:47:45 -0300

    agregando ejercicio01

--- src/ar/edu/unju/fi/ejercicio01/model/Producto.java
+++ src/ar/edu/unju/fi/ejercicio01/model/Producto.java
@@ -1,5 +1,77 @@
 package ar.edu.unju.fi.ejercicio01.model;
 
 public class Producto {
+	private String codigo;
+	private String descripcion;
+	private double precioUnitario;
+	private OrigenFabricacion origenFabricacion;
+	private Categoria categoria;
 
+	// Constructor
+	public Producto(String codigo, String descripcion, double precioUnitario, OrigenFabricacion origenFabricacion,
+			Categoria categoria) {
+		this.setCodigo(codigo);
+		this.setDescripcion(descripcion);
+		this.setPrecioUnitario(precioUnitario);
+		this.setOrigenFabricacion(origenFabricacion);
+		this.setCategoria(categoria);
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public OrigenFabricacion getOrigenFabricacion() {
+		return origenFabricacion;
+	}
+
+	public void setOrigenFabricacion(OrigenFabricacion origenFabricacion) {
+		this.origenFabricacion = origenFabricacion;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	// Enumeración para Origen de Fabricación
+	public enum OrigenFabricacion {
+		ARGENTINA, CHINA, BRASIL, URUGUAY
+	}
+
+	// Enumeración para Categoría
+	public enum Categoria {
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
+	}
+	
+	
+    @Override
+    public String toString() {
+        return "Código: " + codigo + ", Descripción: " + descripcion + ", Precio Unitario: $" + precioUnitario +
+                ", Origen de Fabricación: " + origenFabricacion + ", Categoría: " + categoria;
+    }
+	
 }
